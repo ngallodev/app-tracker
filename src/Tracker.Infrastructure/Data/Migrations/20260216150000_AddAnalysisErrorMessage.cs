515b09ada4b95b3be75098d8243cec95be7d04f8
ngallodev <nathan.gallo@gmail.com>
2026-02-16 19:02:48 -0800

    Add EF migration for Analysis.ErrorMessage column
    - Create migration to add ErrorMessage column to analyses table
    - Update ModelSnapshot to reflect new column

---
+++ src/Tracker.Infrastructure/Data/Migrations/20260216150000_AddAnalysisErrorMessage.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tracker.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAnalysisErrorMessage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ErrorMessage",
+                table: "analyses",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ErrorMessage",
+                table: "analyses");
+        }
+    }
+}
